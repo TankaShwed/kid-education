d582f504131f36c98d0fc9c32fd84419c0607502
Aleksei Shvedyuk <druid-aa@ya.ru>
2026-02-24 19:21:29 +0300

    unused componet

--- src/tasks/pick-syllable/index.ts
+++ src/tasks/pick-syllable/index.ts
@@ -1,4 +1,2 @@
-export { PickSyllableRound } from './PickSyllableRound';
 export { PickSyllableRoundContainer } from './PickSyllableRoundContainer';
 export { pickSyllableSlice } from './pickSyllableSlice';
-export type { PickSyllableRoundProps } from './PickSyllableRound';
